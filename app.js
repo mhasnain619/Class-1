960b128c40f2c2a3992512919a220fe64cab19c2
Muhammad Hasnain <hasnain0893@gmail.com>
2024-11-17 00:08:24 +0500

    first assignment

---
+++ app.js
@@ -0,0 +1,21 @@
+// my biodata 
+
+const name = "Muhammad Hasnain";
+const email = "hasn@example.com";
+const city = "Karachi";
+const education = "Master's in Computer Science";
+const occupation = "Web Developer";
+const instituteName = "Jawan Pakistan";
+let phoneNumber = '+92-362633523'
+
+const bioData = `
+<h1>Biodata</h1>
+<p>Name: ${name}</p>
+<p>Email: ${email}</p>
+<p>City: ${city}</p>
+<p>Education: ${education}</p>
+<p>Occupation: ${occupation}</p>
+<p>Institute Name: ${instituteName}</p>
+<p>Phone Number: ${phoneNumber}</p>
+`
+document.write(bioData)
